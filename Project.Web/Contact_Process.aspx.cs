eaae251216136457ab03677c70a5717777281029
h4sm4n <huseyincetin4@posta.mu.edu.tr>
2021-03-30 21:37:44 +0300

    Template proje üzerine entegre edildi ve kurumsal mimari yapısı oluşturuldu

---
+++ Project.Web/Contact_Process.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Reflection;
+using runnerDotNet;
+
+namespace Project.Web
+{
+    public partial class Contact_Process : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
